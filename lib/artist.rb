46e2351b0611678430efd14df4bb1a4cfdb64793
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-21 00:18:15 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,7 +21,7 @@ class Artist
   end
 
   def self.song_count
-    @songs.collect{|song| @@song_count << song}
+    self.songs.collect{|song| @@song_count << song}
     @@song_count.length
   end
 end
