c60a04bca6bdb8979ced301112beb0204c4e4a63
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-21 00:30:21 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,12 +11,14 @@ class Artist
 
   def add_song(song)
     self.songs << song
+    @@song_count << song
     song.artist = self
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     self.songs << song
+    @@song_count << song
     song.artist = self
   end
 
